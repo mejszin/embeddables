122442f4daccec5d510e382cccf4a993051fd49a
Louis Machin <ldhmachin@gmail.com>
2022-06-13 19:52:53 +0100

    Added helloworld image

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
 
+const { createCanvas, loadImage } = require('canvas')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// create application/json parser
-// this will only handle application/json requests
-// and will be passed to the route handler as middleware
 
 app.locals.jsonParser = bodyParser.json()
 app.locals.urlencodedParser = bodyParser.urlencoded({ extended: true })
@@ -18,6 +16,29 @@ const VERSION = 'v0.0.1';
 
 app.get('/ping', (req, res) => {
     res.status(200).send('Pong!');
+});
+
+app.get('/helloworld', (req, res) => {
+    const canvas = createCanvas(200, 200)
+    const ctx = canvas.getContext('2d')
+    // Write "Awesome!"
+    ctx.font = '30px Impact'
+    ctx.rotate(0.1)
+    ctx.fillText('Hello world!', 40, 100)
+    // Draw line under text
+    var text = ctx.measureText('Awesome!')
+    ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+    ctx.beginPath()
+    ctx.lineTo(50, 102)
+    ctx.lineTo(50 + text.width, 102)
+    ctx.stroke()
+    // Create outputs
+    var img = Buffer.from(canvas.toBuffer(), 'base64');
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': img.length
+    });
+    res.end(img);
 });
 
 app.listen(PORT, () => console.log(`It's alive on port ${PORT}!`));
